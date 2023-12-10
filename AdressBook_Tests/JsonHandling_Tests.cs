8d2fbef73c82b81caa74bff5558ef718d9297fc5
Ted Pieplow <ted.pieplow@gmail.com>
2023-12-10 12:30:18 +0100

    Added test for JsonReader

---
+++ AdressBook_Tests/JsonHandling_Tests.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibrary_AdressBook.Interfaces;
+using ClassLibrary_AdressBook.Models;
+using ClassLibrary_AdressBook.Services;
+using Moq;
+
+namespace AdressBook_Tests;
+
+public class JsonHandling_Tests
+{
+    [Fact]
+    public void JsonReader_Should_ReadAFile_ToApplication()
+    {
+        // Arrange
+        var readerMock = new Mock<IJsonReader>();
+        List<IContact> expectedContacts = new List<IContact>()
+        {
+            new Contact { FirstName = "Ted", LastName = "Pieplow", Email = "ted.pieplow@gmail.com"}
+        };
+        readerMock.Setup(r => r.LoadFromFile("testfile.json")).Returns(expectedContacts);
+
+        // Act
+        IEnumerable<IContact> actualContacts = readerMock.Object.LoadFromFile("testfile.json");
+
+        // Assert
+
+        Assert.Equal(expectedContacts, actualContacts);
+    }
+
+    [Fact]
+    public void JsonWriter_Should_WriteAFile_ToFolder()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
