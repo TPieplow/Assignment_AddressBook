22e8663af03903359dae013bf93b964c7ea862fd
Ted Pieplow <ted.pieplow@gmail.com>
2023-12-14 10:08:48 +0100

    Added comments for consoleapp contactservice

--- ConsoleApp_AdressBook/Interfaces/IContactServiceConsoleApp.cs
+++ ConsoleApp_AdressBook/Interfaces/IContactServiceConsoleApp.cs
@@ -2,10 +2,29 @@
 {
     public interface IContactServiceConsoleApp
     {
+        /// <summary>
+        /// Method that takes userinput and adds it to list (calling AddContact in classlibrary and save)
+        /// </summary>
         void AddContactConsoleApp();
+
+        /// <summary>
+        /// Method that takes email from userinput and lotaces a contact to delete
+        /// </summary>
         void DeleteContact();
+
+        /// <summary>
+        /// Method that shows all contacts for the user
+        /// </summary>
         void ShowAllContactDetails();
+
+        /// <summary>
+        /// Method that displays one single contact, located by email
+        /// </summary>
         void ShowContactDetails();
+
+        /// <summary>
+        /// Method that lets the user update a existing contact, using email to locate contact
+        /// </summary>
         void UpdateContact();
     }
 }
