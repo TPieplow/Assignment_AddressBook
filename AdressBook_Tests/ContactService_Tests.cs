0cfdd904b2cdf6bf4835a7c7d1be5d2371a43296
Ted Pieplow <ted.pieplow@gmail.com>
2023-12-14 09:20:19 +0100

    Updatecontact test approved

--- AdressBook_Tests/ContactService_Tests.cs
+++ AdressBook_Tests/ContactService_Tests.cs
@@ -120,28 +120,30 @@ public class ContactService_Tests
         // Assert
         Assert.NotNull(result);
     }
+
+
+    [Fact]
+    public void UpdateContact_Should_UpdateCurrentContact()
+    {
+        // Arrange
+        var writerMock = new Mock<IJsonWriter>();
+        var readerMock = new Mock<IJsonReader>();
+        IContactService contactService = new ContactService(writerMock.Object, readerMock.Object);
+        IContact initialContact = new Contact { FirstName = "InitialContact", LastName = "Last name", Email = "test@test.com" };
+        contactService.AddContact(initialContact);
+
+        IContact updatedContact = new Contact { FirstName = "UpdatedContact", LastName = "Last name", Email = "test@test.com" };
+
+        // Act
+        contactService.UpdateContact(updatedContact);
+
+        // Assert
+        IContact result = contactService.GetContact("test@test.com");
+
+        Assert.NotNull(result);
+        Assert.Equal("UpdatedContact", result.FirstName);
+    }
 }
-
-//    [Fact]
-//    public void UpdateContact_Should_UpdateCurrentContact()
-//    {
-//        // Arrange
-//        var writerMock = new Mock<IJsonWriter>();
-//        IContactService contactService = new ContactService(writerMock.Object, new List<IContact>());
-//        IContact initialContact = new Contact { FirstName = "Initial", LastName = "Last name", Email = "test@test.com" };
-//        contactService.AddContact(initialContact);
-
-//        IContact updatedContact = new Contact { FirstName = "Updated", LastName = "Last name", Email = "test@test.com" };
-
-//        // Act
-//        contactService.UpdateContact(updatedContact, "whateverfilename");
-
-//        // Assert
-//        IContact result = contactService.GetContact("test@test.com");
-
-//        Assert.NotNull(result);
-//        Assert.Equal("Updated", result.FirstName);
-//    }
 
 //    [Fact]
 //    public void RemoveContact_Should_RemoveContact_UsingEmail()
