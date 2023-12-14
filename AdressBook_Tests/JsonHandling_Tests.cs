76a5937e48489f2329646f68bc24ac322c05a481
Ted Pieplow <ted.pieplow@gmail.com>
2023-12-14 08:49:24 +0100

    Rewrote tests since application had some major changes. JsonHandling_Tests Pass

--- AdressBook_Tests/JsonHandling_Tests.cs
+++ AdressBook_Tests/JsonHandling_Tests.cs
@@ -1,45 +1,46 @@
-﻿//using ClassLibrary_AdressBook.Interfaces;
-//using ClassLibrary_AdressBook.Models;
-//using Moq;
+﻿using ClassLibrary_AdressBook.Interfaces;
+using ClassLibrary_AdressBook.Models;
+using Moq;
 
-//namespace AdressBook_Tests;
+namespace AdressBook_Tests;
 
-//public class JsonHandling_Tests
-//{
-//    [Fact]
-//    public void JsonReader_Should_ReadAFile_ToApplication()
-//    {
-//        // Arrange
-//        var readerMock = new Mock<IJsonReader>();
-//        List<IContact> expectedContacts = new List<IContact>()
-//        {
-//            new Contact { FirstName = "Ted", LastName = "Pieplow", Email = "ted.pieplow@gmail.com"}
-//        };
-//        readerMock.Setup(r => r.LoadFromFile("testfile.json")).Returns(expectedContacts);
+public class JsonHandling_Tests
+{
+    [Fact]
+    public void JsonReader_Should_ReadAFile_ToApplication()
+    {
+        // Arrange
+        var readerMock = new Mock<IJsonReader>();
+        List<IContact> expectedContacts = new List<IContact>()
+        {
+            new Contact { FirstName = "Ted", LastName = "Pieplow", Email = "ted.pieplow@gmail.com"}
+        };
+        readerMock.Setup(r => r.LoadFromFile()).Returns(expectedContacts);
 
-//        // Act
-//        IEnumerable<IContact> actualContacts = readerMock.Object.LoadFromFile("testfile.json");
+        // Act
+        IEnumerable<IContact> actualContacts = readerMock.Object.LoadFromFile();
 
-//        // Assert
+        // Assert
 
-//        Assert.Equal(expectedContacts, actualContacts);
-//    }
+        Assert.Equal(expectedContacts, actualContacts);
+    }
 
-//    [Fact]
-//    public void JsonWriter_Should_WriteAFile_ToFolder()
-//    {
-//        // Arrange
-//        var writerMock = new Mock<IJsonWriter>();
-//        List<IContact> savedContacts = new List<IContact>()
-//        {
-//            new Contact { FirstName = "Ted", LastName = "Pieplow", Email = "ted.pieplow@gmail.com"}
-//        };
-//        writerMock.Setup(w => w.SaveToFile(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<List<IContact>>()));
 
-//        // Act
-//        writerMock.Object.SaveToFile("testfile.json", "2", savedContacts);
+    [Fact]
+    public void JsonWriter_Should_WriteAFile_ToFolder()
+    {
+        // Arrange
+        var writerMock = new Mock<IJsonWriter>();
+        List<IContact> savedContacts = new List<IContact>()
+        {
+            new Contact { FirstName = "Ted", LastName = "Pieplow", Email = "ted.pieplow@gmail.com"}
+        };
+        writerMock.Setup(w => w.SaveToFile(It.IsAny<List<IContact>>()));
 
-//        // Assert
-//        writerMock.Verify(w => w.SaveToFile("testfile.json", "2", savedContacts), Times.Once);
-//    }
-//}
+        // Act
+        writerMock.Object.SaveToFile(savedContacts);
+
+        // Assert
+        writerMock.Verify(w => w.SaveToFile(savedContacts), Times.Once);
+    }
+}
