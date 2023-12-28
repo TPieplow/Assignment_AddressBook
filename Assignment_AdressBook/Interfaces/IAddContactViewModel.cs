d52a0f32f9c1d197dfb4bd4d2fcde1378ec7c5f8
Ted Pieplow <ted.pieplow@gmail.com>
2023-12-28 20:43:49 +0100

    Added a comment

--- Assignment_AdressBook/Interfaces/IAddContactViewModel.cs
+++ Assignment_AdressBook/Interfaces/IAddContactViewModel.cs
@@ -4,7 +4,19 @@ namespace Assignment_AdressBook.Interfaces;
 
 public interface IAddContactViewModel
 {
+
+    /// <summary>
+    /// Eventhandler reacting to newly added contacts
+    /// </summary>
     event EventHandler<ContactAddedEventArgs> ContactAdded;
+
+    /// <summary>
+    /// Adds a contact via user input
+    /// </summary>
     void AddContact();
+
+    /// <summary>
+    /// Takes the user back to main menu
+    /// </summary>
     void BackToMenu();
 }
